fac27d8417ec8706932be78814028744f89832d2
”Alex <Air@MacBook-Air-Air.local>
2017-02-09 20:07:03 +0200

    new venv was implemented

--- src/studentsdb/studentsdb/urls.py
+++ src/studentsdb/studentsdb/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from students import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Students urls
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.students_list, name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
