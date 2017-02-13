940e5d7dc7bfb7da4c1ea1c00c0a411d9443478d
”Alex <Air@MacBook-Air-Air.local>
2017-02-13 17:41:59 +0200

    groups_list & studens_list inheritage

--- src/studentsdb/students/views.py
+++ src/studentsdb/students/views.py
@@ -41,14 +41,25 @@ def students_delete(request, sid):
     # Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+
+    groups = (
+        {'id': 1,
+         'name': u'ІПС-2',
+         'leader': {'id': 1, 'name': u'Арістотель Хтосько'}},
+        {'id': 1,
+         'name': u'ІПС-3',
+         'leader': {'id': 1, 'name': u'Новий Василь'}},
+    )
+
+    return render(request, 'students/groups_list.html',
+                  {"groups": groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
 
-def groups_edit(request, sid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % sid)
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
 
-def groups_delete(request, sid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % sid)
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
 
