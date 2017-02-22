73a61565ddd3e2d77fd9e88605548f6532305ff6
”Alex <Alex_Jr@MacBook-Air-Air.local>
2017-02-22 15:27:42 +0200

    navigation in students_list was made

--- src/studentsdb/students/views.py
+++ src/studentsdb/students/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -18,6 +19,15 @@ def students_list(request):
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
 
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        students = paginator.page(1)
+    except EmptyPage:
+        students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html',
                   {'students': students})
